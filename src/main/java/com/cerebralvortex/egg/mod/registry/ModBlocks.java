f934190264b2d79c6adda44f9f7eed7c1ad0a5aa
Meghan Lin <mlin2024@gmail.com>
2021-06-21 21:07:12 -0400

    'PISSgit add .' *ding*

--- src/main/java/com/cerebralvortex/egg/mod/registry/ModBlocks.java
+++ src/main/java/com/cerebralvortex/egg/mod/registry/ModBlocks.java
@@ -4,12 +4,18 @@ import com.cerebralvortex.egg.mod.EggMod;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
 import net.minecraft.block.Material;
+import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
+	public static FlowableFluid STILL_YOLK;
+	public static FlowableFluid FLOWING_YOLK;
+	public static Block YOLK;
 
 	public static final EggBlock EGG_BLOCK = new EggBlock(FabricBlockSettings
 			.of(Material.EGG) // Sound when placed/broken
@@ -18,5 +24,10 @@ public class ModBlocks {
 	
 	public static void registerBlocks() {
 		Registry.register(Registry.BLOCK, new Identifier(EggMod.MOD_ID, "egg_block"), EGG_BLOCK);
+		
+		// Yolk
+		STILL_YOLK = Registry.register(Registry.FLUID, new Identifier(EggMod.MOD_ID, "yolk_flow"), new YolkFluid.Still());
+		FLOWING_YOLK = Registry.register(Registry.FLUID, new Identifier(EggMod.MOD_ID, "yolk_still"), new YolkFluid.Flowing());
+		YOLK = Registry.register(Registry.BLOCK, new Identifier(EggMod.MOD_ID, "yolk"), new FluidBlock(STILL_YOLK, FabricBlockSettings.copy(Blocks.WATER)){});
 	}
 }
--- src/main/java/com/cerebralvortex/egg/mod/registry/ModItems.java
+++ src/main/java/com/cerebralvortex/egg/mod/registry/ModItems.java
@@ -2,13 +2,17 @@ package com.cerebralvortex.egg.mod.registry;
 
 import com.cerebralvortex.egg.mod.EggMod;
 
+import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+	public static Item YOLK_BUCKET;
 	
 	// Items
 	public static final SpecialEgg SPECIAL_EGG = new SpecialEgg(new Item.Settings().group(EggMod.EGG_ITEMS).maxCount(16));
@@ -21,13 +25,19 @@ public class ModItems {
 	// Blocks
 	public static final BlockItem EGG_BLOCK = new BlockItem(ModBlocks.EGG_BLOCK, new Item.Settings().group(EggMod.EGG_ITEMS));
 	
+	
 	public static void registerItems() {
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "special_egg"), SPECIAL_EGG);
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_block"), EGG_BLOCK);
+		
+		// Tools
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_pickaxe"), EGG_PICKAXE);
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_axe"), EGG_AXE);
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_sword"), EGG_SWORD);
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_hoe"), EGG_HOE);
 		Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "egg_shovel"), EGG_SHOVEL);
+		
+		// Yolk
+		YOLK_BUCKET = Registry.register(Registry.ITEM, new Identifier(EggMod.MOD_ID, "yolk_bucket"), new BucketItem(ModBlocks.STILL_YOLK, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1).group(EggMod.EGG_ITEMS)));
 	}
 }
